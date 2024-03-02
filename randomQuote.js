2a0d8bc747d7de4148a609d24e8f22c2f2cbac14
amartyasen19 <134506668+amartyasen19@users.noreply.github.com>
2024-03-02 21:11:40 +0530

    Add files via upload

---
+++ randomQuote.js
@@ -0,0 +1,17 @@
+const quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
+
+const url = "https://api.quotable.io/random";
+let getQuote = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      console.log(item.author);
+      console.log(item.content);
+      quote.innerText = item.content;
+      author.innerText = item.author;
+    });
+};
+window.addEventListener("load", getQuote);
+btn.addEventListener("click", getQuote);
